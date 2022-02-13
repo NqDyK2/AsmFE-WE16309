fe1e8699b66e911b60c25f48e5bf93832895d1a4
NqDyK2 <88083327+NqDyK2@users.noreply.github.com>
2022-02-14 00:30:58 +0700

    feat: step2. Next step just update new interface of asm.

--- source-Asm/main.js
+++ source-Asm/main.js
@@ -1,23 +1,44 @@
 import Navigo from "navigo";
+import AboutPage from "./pages/about";
+import Dashboard from "./pages/admin/dashboard";
+import AdminNewsPage from "./pages/admin/news";
+import AdminAddPost from "./pages/admin/news/add";
+import AdminEditPost from "./pages/admin/news/edit";
+import DetailPage from "./pages/detail";
+import HomePage from "./pages/home";
+import ProductPage from "./pages/product";
+import Signin from "./pages/signin";
+import Signup from "./pages/signup";
 
 const router = new Navigo("/", { linksSelector: "a", hash: true });
 
 const print = async (content, id) => {
-    document.querySelector("#Asm").innerHTML = await content.render(id);
+    document.querySelector("#app").innerHTML = await content.render(id);
     if (content.afterRender) content.afterRender(id);
 };
 router.on("/admin/*", () => {}, {
     before: (done) => {
         const userId = JSON.parse(localStorage.getItem("user")).id;
+        // nếu userId === 1 thì tôi mới render
         if (userId === 1) {
             done();
         } else {
+            // ngược thì lại redirect về trang chủ
             document.location.href = "/";
         }
     },
 });
 router.on({
-    "/": () => print(),
-    // .................
+    "/": () => print(HomePage),
+    "/about": () => print(AboutPage),
+    "/product": () => print(ProductPage),
+    "/news/:id": ({ data }) => print(DetailPage, data.id),
+    "/admin/dashboard": () => print(Dashboard),
+    "/admin/news": () => print(AdminNewsPage),
+    "/admin/news/add": () => print(AdminAddPost),
+    "/admin/news/:id/edit": ({ data }) => print(AdminEditPost, data.id),
+    "/signup": () => print(Signup),
+    "/signin": () => print(Signin),
 });
+
 router.resolve();
