ca3ba0873cf0e222c27a54db31818270548570f3
NqDyK2 <88083327+NqDyK2@users.noreply.github.com>
2022-02-16 00:13:30 +0700

    Feat: update more function add to cart

--- src/components/header.js
+++ src/components/header.js
@@ -4,18 +4,20 @@ import "toastr/build/toastr.min.css";
 
 const Header = {
     render() {
-        return /* html */`
+        return `
         <div class="bg-orange-500 flex justify-between items-center">
             <ul class="flex">
-                <li><a href="/" class="block px-4 py-5 hover:bg-indigo-500 hover:text-white">Home Page</a></li>
-                <li><a href="/#/about" class="block px-4 py-5 hover:bg-indigo-500 hover:text-white">About Page</a></li>
-                <li><a href="/#/product" class="block px-4 py-5 hover:bg-indigo-500 hover:text-white">Product Page</a></li>
-                <li><a href="/#/admin/dashboard" class="block px-4 py-5 hover:bg-indigo-500 hover:text-white ">Dashboard Page</a></li>
+            <li><a href="/" class="block px-4 py-5 hover:bg-indigo-500 hover:text-white">Home Page</a></li>
+            <li><a href="/#/about" class="block px-4 py-5 hover:bg-indigo-500 hover:text-white">About Page</a></li>
+            <li><a href="/#/products" class="block px-4 py-5 hover:bg-indigo-500 hover:text-white">Product Page</a></li>
+            <li><a href="/#/admin/dashboard" class="block px-4 py-5 hover:bg-indigo-500 hover:text-white">Dashboard Page</a></li>
+           
             </ul>
             ${localStorage.getItem("user") ? `<ul class="flex">
-                <li><a  id="account-email" class="block px-4 py-5 hover:bg-indigo-500 hover:text-white"></a></li>
-                <li><a  id="logout" class="block px-4 py-5 hover:bg-indigo-500 hover:text-white cursor-pointer" >Logout</a></li>
-            </ul>` : ""}
+            <li><a  id="account-email" class="block px-4 py-5 hover:bg-indigo-500 hover:text-white"></a></li>
+            <li><a  id="logout" class="block px-4 py-5 hover:bg-indigo-500 hover:text-white cursor-pointer">Logout</a></li>
+        </ul>` : ""}
+            
         </div>
         `;
     },
@@ -26,7 +28,6 @@ const Header = {
         document.querySelector("#account-email").innerHTML = user.email;
         // logout
         logout.addEventListener("click", () => {
-            // eslint-disable-next-line no-undef
             toastr.success("Logout thành công");
             localStorage.removeItem("user");
             reRender(Header, "#header");
