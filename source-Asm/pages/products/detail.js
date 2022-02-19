9a008e42c01308e105377eb6ff4cb6c6c2793390
NqDyK2 <88083327+NqDyK2@users.noreply.github.com>
2022-02-20 00:54:06 +0700

    feat: update add quantity for add to cart

--- source-Asm/pages/products/detail.js
+++ source-Asm/pages/products/detail.js
@@ -215,7 +215,10 @@ const DetailProductPage = {
                 </label>
               </div>
             </fieldset>
-            <input type="number" id="inputValue" />
+            <input type="number" id="inputValue" class="border-neutral-500 focus:ring-indigo-500 focus:border-indigo-500 block w-full mt-5 pl-7 pr-12 sm:text-sm  border-orange-200 rounded-md"/>
+            <div>
+           
+          </div>
           <button id="btnAddToCart" class="mt-10 w-full bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Add to bag</button>
           </div>
 
